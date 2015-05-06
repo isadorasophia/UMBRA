cfa3e4b3032450b417a196588364b564f7e33c57
Isadora <isaecia@gmail.com>
2015-05-05 21:09:50 -0300

    First attempt to load the project.

---
+++ Awesome/desktop/src/com/umbra/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.umbra.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.umbra.Umbra;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Umbra(), config);
+	}
+}
