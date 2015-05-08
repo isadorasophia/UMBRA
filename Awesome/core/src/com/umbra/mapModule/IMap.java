fe777ac5b49d48dc3b03634ae72ceae7f808ba36
Lauro Cruz <laurocruzsouza@gmail.com>
2015-05-08 20:59:53 -0300

    Interface IMap criada, mas com getMap incompleto

---
+++ Awesome/core/src/com/umbra/mapModule/IMap.java
@@ -0,0 +1,11 @@
+package com.umbra.mapModule;
+
+/**
+ * Created by laurocruz on 5/8/15.
+ */
+public interface IMap {
+    public void initialPosition();
+    public void getPosition(IPosition posicao);
+    public void move(IPosition destino);
+    public void getMap(IPosition );
+}
