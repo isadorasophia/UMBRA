110a25d92535f1efe4c097d1a2de7ed9c933a715
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-05-09 01:14:48 -0300

    correcao map

--- Awesome/core/src/com/umbra/mapModule/IMap.java
+++ Awesome/core/src/com/umbra/mapModule/IMap.java
@@ -5,5 +5,5 @@ import com.umbra.mobModule.mobComponent.IMob;
 public interface IMap {
     public void initialPosition(IMob player);
     public void getPosition(IPosition posicao);
-    public void move(IPosition destino);
+    public boolean move(IMob entidade, char direction);
 }
