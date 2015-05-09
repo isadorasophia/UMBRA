d56bfd729411b6cce1757821a30faf9a78a2c621
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-05-08 22:20:27 -0300

    Inicio implementação map

--- Awesome/core/src/com/umbra/mapModule/Map.java
+++ Awesome/core/src/com/umbra/mapModule/Map.java
@@ -1,15 +1,31 @@
 package com.umbra.mapModule;
 
-public class Map extends IMap{
+import com.umbra.mobModule.mobComponent.IMob;
+import java.util.Random;
 
+public class Map implements IMap{
     private ICell[][] corredor;
 
-    {
-        corredor = new ICell[5][20];
+    corredor = new ICell[25][5];
+
+    @Override
+    public void initialPosition(IMob player) {
+        int cont = 0;
+
+        for(int i = 0;i < 25; i++) {
+            for(int j = 0; j < 5; j++) {
+
+            }
+        }
     }
 
     @Override
-    public void initialPosition() {
+    public void getPosition(IPosition posicao) {
+        return;
+    }
+
+    @Override
+    public void move(IPosition destino) {
 
     }
 }
