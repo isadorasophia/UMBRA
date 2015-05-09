de6c85ea480ec3ceccbe6601f7cbcbfcccdc63dd
Gabriel Henriques siqueira <gabriel._gabriel@hotmail.com>
2015-05-09 02:10:58 -0300

    Delete ISelector.java

--- Awesome/core/src/com/umbra/maneger/ISelector.java
+++
@@ -1,8 +0,0 @@
-package com.umbra.maneger;
-
-public interface ISelector {
-    public void setMode(Modes state);
-    public void update(float dt);
-    public void draw();
-    public void dispose();
-}
