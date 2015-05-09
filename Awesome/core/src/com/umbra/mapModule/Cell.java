a4110f00b1f9798d3852b0f02ca3861b5f214dfa
Lauro Cruz <laurocruzsouza@gmail.com>
2015-05-08 21:22:24 -0300

    Começado o Cell e mudado do nome de iCell para ICell

--- Awesome/core/src/com/umbra/mapModule/Cell.java
+++ Awesome/core/src/com/umbra/mapModule/Cell.java
@@ -1,4 +1,18 @@
 package com.umbra.mapModule;
 
+import com.umbra.mobModule.mobComponent.IMob;
+import com.umbra.puzzlesModule.IPuzzle;
+
 public class Cell {
+    private IMob entidade;
+    private IPuzzle porta;
+    private String descricao;
+
+    public void receive
+
+    public void remove (IMob entidade) {
+
+
+    }
+
 }
--- Awesome/core/src/com/umbra/mapModule/ICell.java
+++ Awesome/core/src/com/umbra/mapModule/ICell.java
@@ -3,7 +3,7 @@ package com.umbra.mapModule;
 import com.umbra.mobModule.mobComponent.IMob;
 import com.umbra.puzzlesModule.IPuzzle;
 
-public interface iCell {
+public interface ICell {
     public void remove(IMob entidade);
     public void receive(IMob entidade);
     public IMob getEntety();
