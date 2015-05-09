2860144287f0231ed96819491fa867b642908b61
Gabriel-Siqueira <gabriel._gabriel@hotmail.com>
2015-05-08 23:40:16 -0300

    desktop definitions

--- Awesome/core/src/com/umbra/maneger/MazeMode.java
+++ Awesome/core/src/com/umbra/maneger/MazeMode.java
@@ -1,9 +1,23 @@
 package com.umbra.maneger;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class MazeMode implements IMode{
+    private boolean beginning;
+    private String initialText;
+    private BufferedReader reader;
+    private static String dir = MazeMode.class.getResource(".").getPath() + "/";
+
     @Override
     public void init() {
-
+        try {
+            reader = new BufferedReader(new FileReader("Textos/initialText.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        beginning = true;
     }
 
     @Override
@@ -13,7 +27,11 @@ public class MazeMode implements IMode{
 
     @Override
     public void draw() {
+        if(beginning){
 
+        }else{
+
+        }
     }
 
     @Override
