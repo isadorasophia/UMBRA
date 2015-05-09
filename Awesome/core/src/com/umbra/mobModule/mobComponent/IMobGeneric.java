b5ba4366e01bcce1c33e9e38939cc6ade17ae4d0
Lucas <racoci.0@gmail.com>
2015-05-09 03:23:41 -0300

    Metodo get Description

--- Awesome/core/src/com/umbra/mobModule/mobComponent/IMobGeneric.java
+++ Awesome/core/src/com/umbra/mobModule/mobComponent/IMobGeneric.java
@@ -7,6 +7,7 @@ import com.umbra.mapModule.IPosition;
 public interface IMobGeneric {
     public String getName();
     public IAttribute getAtt(String type);
+    public String getDescription();
     public void setAtt(String type, double value);
     public void putAtt(String attName, double attValue);
     public Boolean hasAtt(String name);
