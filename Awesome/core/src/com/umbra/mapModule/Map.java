895636552909405a92485523e4e17d5f543363b4
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-05-08 21:33:47 -0300

    Position

--- Awesome/core/src/com/umbra/mapModule/Map.java
+++ Awesome/core/src/com/umbra/mapModule/Map.java
@@ -1,4 +1,15 @@
 package com.umbra.mapModule;
 
-public class Map {
+public class Map extends IMap{
+
+    private ICell[][] corredor;
+
+    {
+        corredor = new ICell[5][20];
+    }
+
+    @Override
+    public void initialPosition() {
+
+    }
 }
--- Awesome/core/src/com/umbra/mapModule/Position.java
+++ Awesome/core/src/com/umbra/mapModule/Position.java
@@ -1,5 +1,34 @@
 package com.umbra.mapModule;
 
 public class Position implements IPosition{
+    public int posX;
+    public int posY;
 
+    public Boolean moveNorth(){
+        if(ValidMove("north"))
+            this.posY--;
+        return true;
+        return false;
+    }
+
+    public Boolean moveSouth(){
+        if(ValidMove("south"))
+            this.posY++;
+        return true;
+        return false;
+    }
+
+    public Boolean moveWest(){
+        if(ValidMove("west"))
+            this.posX--;
+        return true;
+        return false;
+    }
+
+    public Boolean moveEast(){
+        if(ValidMove("east"))
+            this.posX++;
+        return true;
+        return false;
+    }
 }
