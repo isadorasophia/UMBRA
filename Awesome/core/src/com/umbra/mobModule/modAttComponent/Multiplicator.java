dda45cfec9a40f686c2c6f259b77e04c490b7388
Racoci <LRacoci@users.noreply.github.com>
2015-05-19 13:50:44 -0300

    bla

--- Awesome/core/src/com/umbra/mobModule/modAttComponent/Multiplicator.java
+++ Awesome/core/src/com/umbra/mobModule/modAttComponent/Multiplicator.java
@@ -4,7 +4,6 @@ package com.umbra.mobModule.modAttComponent;
 import com.umbra.mobModule.attComponent.IAttribute;
 
 public class Multiplicator implements IModificator {
-    /* Throws BadArgumentException if(parameter[0] == 0) */
     public IAttribute modify(IAttribute src, double... parameter) {
         src.setValue(src.getValue() * parameter[0]);
         return src;
