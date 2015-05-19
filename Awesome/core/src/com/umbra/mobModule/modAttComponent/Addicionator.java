3481c86360d68d96520d55cfb14a1b9e130caafd
Racoci <LRacoci@users.noreply.github.com>
2015-05-19 13:43:57 -0300

    bla

--- Awesome/core/src/com/umbra/mobModule/modAttComponent/Addicionator.java
+++ Awesome/core/src/com/umbra/mobModule/modAttComponent/Addicionator.java
@@ -5,7 +5,6 @@ import com.umbra.mobModule.attComponent.IAttribute;
 
 public class Addicionator implements IModificator {
 
-    /* Throws CannotDoubleModifyAnAttribute if(src != null) */
     public IAttribute modify(IAttribute src, double... parameter) {
         src.setValue(src.getValue() + parameter[0]);
         return src;
