25b1ba7d9f4faf586b5285750cf9899dce25f18a
Isadora <isaecia@gmail.com>
2015-05-25 11:14:54 -0300

    made some changes on battle executer

--- Awesome/core/src/com/umbra/battleModule/BattleExecuter.java
+++ Awesome/core/src/com/umbra/battleModule/BattleExecuter.java
@@ -1,32 +1,85 @@
 package com.umbra.battleModule;
 
+import java.util.Random;
+
 import com.umbra.mobModule.mobComponent.*;
 
 // Static class, which manages the battle AI
-final class BattleExecuter {
+class BattleExecuter {	
+	private static enum AttackState { normal, critical, counter, missed }
+	
+	// BodyPart arm, leg, belly, head;
+	
 	// Prevents to instantiate the class, since it's supposed to be static
 	private BattleExecuter () { }
+	private static BattleExecuter instance = null;
+	
+	public static BattleExecuter getInstance() {
+		if (instance == null) {
+			instance = new BattleExecuter();
+		}
+		
+		return instance;
+	}
 	
 	// Tries to escape!
-	static boolean escape (IMobGeneric player, IMobGeneric enemy) {
+	boolean escape (IMob player, IMob enemy) {
 		return false;
 	}
 	
-	static String attack (IMobGeneric attacker, IMobGeneric victim) {
+	String attack (IMob attacker, IMob victim) {
+		float damage;
+		AttackState attackState = stateOfAttack (attacker, victim);
+		
+		switch (attackState) {
+			case normal:
+				
+			case critical:
+				
+			case counter:
+				return "Counter attack!\n" + attack (victim, attacker);
+			case missed:
+				// TODO: name for IMob
+				return attacker.getName() + "tried to attack" + victim.getName() + "but missed!\n";
+		}
+		
 		return null;
 	}
 	
-	static String defend (IMobGeneric attacker, IMobGeneric victim) {
+	private AttackState stateOfAttack (IMob attacker, IMob victim) {
+		Random random = new Random();
+		double attackerRate = attacker.getAtt("speed").getValue() + attacker.getAtt("dexterity").getValue() + attacker.getAtt("luck").getValue();
+		double enemyRate = victim.getAtt("evasiveness").getValue() + victim.getAtt("dexterity").getValue() + victim.getAtt("luck").getValue();
+		
+		// chance to add up to 50% more to their stats
+		attackerRate *= random.nextFloat() * 0.5 + 0.1;
+		enemyRate *= random.nextFloat() * 0.5 + 0.1;
+		
+		if (attackerRate >= enemyRate) {
+			if (attackerRate >= enemyRate * 3)
+				return AttackState.critical;
+			else
+				return AttackState.normal;
+		}
+		else {
+			if (enemyRate > attackerRate * 2)
+				return AttackState.counter;
+			else
+				return AttackState.missed;
+		}
+	}
+	
+	String defend (IMob attacker, IMob victim) {
 		return null;
 	}
 	
 	// Monsters turn
-	static String monsterAI (IMobGeneric monster, IMobGeneric victim) {
+	String monsterAI (IMob monster, IMob victim) {
 		return null;
 	}
 	
 	// Returns if the battle has ended
-	static boolean isItOver (IMobGeneric monster, IMobGeneric victim) {
+	static boolean isItOver (IMob monster, IMob victim) {
 		return false;
 	}
 }
