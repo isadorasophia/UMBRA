202915dc51fd4d6f2ef2b4ad9b02b1142912521f
Lucas Schanner <lsschanner@gmail.com>
2015-05-26 16:22:39 -0300

    Adiciona comentário 100% necessário

--- Awesome/core/src/com/umbra/mobModule/mobComponent/impl/FabricaDeMonstro.java
+++ Awesome/core/src/com/umbra/mobModule/mobComponent/impl/FabricaDeMonstro.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
+/* VEEM MONXSTRO */
 public class FabricaDeMonstro extends MobFactory {
     private static int id = 0;
 
