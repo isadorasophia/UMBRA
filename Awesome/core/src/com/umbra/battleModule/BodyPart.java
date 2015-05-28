68e8fbce0331014da6811c860e2497babb9b5dcd
Isadora Sophia <isaecia@gmail.com>
2015-05-27 21:58:12 -0300

    yayyy moar work is done, yet to do: finishing the battle, balance game, monster AI

--- Awesome/core/src/com/umbra/battleModule/BodyPart.java
+++ Awesome/core/src/com/umbra/battleModule/BodyPart.java
@@ -3,29 +3,21 @@ package com.umbra.battleModule;
 public class BodyPart {
     // By how much the final attack will increase if it succeeds (1 ~ 2)
     private float attackFactor;
+    
     // Chance of succeeding the attack on the body part
     private float hitChance;
+    
     // The name of the bodyPart
     private String bodyPart;
-
-
+    
+    // access functions
+    public float getAttFactor () { return this.attackFactor; }
+    public float getHitChance () { return this.hitChance; }
+    public String getBodyPart () { return this.bodyPart; }
 
     public BodyPart (String bodyPart, float attackFactor, float hitChance) {
         this.attackFactor = attackFactor;
         this.hitChance = hitChance;
         this.bodyPart = bodyPart;
     }
-
-    public float getAttFactor () {
-        return this.attackFactor;
-    }
-
-    public float getHitChance () {
-        return this.hitChance;
-    }
-
-    public String getBodyPart () {
-        return this.bodyPart;
-    }
-
 }
