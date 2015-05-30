86943a0139748d1d03ae575a95241407699d4b40
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-05-30 20:31:14 -0300

    Criacao de um construtor para o dbModule

--- Awesome/core/src/com/umbra/dbModule/dbReader.java
+++ Awesome/core/src/com/umbra/dbModule/dbReader.java
@@ -2,22 +2,39 @@ package com.umbra.dbModule;
 
 import java.io.*;
 
-public class dbReader {
+public class umbraBase{
+    private String path;
+    private File file;
 
+    // Construtor do banco de dados
+    public umbraBase(String name_of_file){
+        path = "core/dataBase/" + name_of_file;
+
+        try {
+            file = new File(path);
+            if( !file.exists() ) {
+                file.createNewFile();
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        //Caso tenha ddo algum outro erro
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     //Talvez fazer um factory que a primeira vez que o cara abrir um db ele ficar aberto?
 
-    private static String openFile(String path, String info){
+    private String readDb(String info){
         //Abre o arquivo em disco
-        File file = new File(path);
         FileInputStream openedFile = null;
         //Faz o arquivo em disco ir para a memória para agilizar a leitura
         BufferedReader bufferedFile = null;
-
         String linha;
         String[] valores;
 
         try {
-
             openedFile = new FileInputStream(file);
             bufferedFile = new BufferedReader(openedFile);
 
