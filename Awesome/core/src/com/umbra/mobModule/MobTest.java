460a7f40d1fbb09f374c66fe67d7e9aaf215dea8
Racoci <racoci.0@gmail.com>
2015-06-06 21:18:02 -0300

    Continuando o gerador de itens aleatórios

--- Awesome/core/src/com/umbra/mobModule/MobTest.java
+++ Awesome/core/src/com/umbra/mobModule/MobTest.java
@@ -12,6 +12,7 @@ import com.umbra.mobModule.mobComponent.inter.IPlayer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MobTest {
@@ -30,7 +31,7 @@ public class MobTest {
             IPlayer player = mobmanager.createPlayer();
             List<IMonstro> monstros = new ArrayList<IMonstro>();
 
-            System.out.print("Digite o número de monstros a serem criados:");
+            /*System.out.print("Digite o número de monstros a serem criados:");
             int n = s.nextInt();
             int i = 1;
             while(n-- > 0){
@@ -38,7 +39,21 @@ public class MobTest {
                 IMonstro novo = mobmanager.createMonstro(i++);
                 System.out.println(novo.toString());
                 monstros.add(novo);
+            }*/
+            System.out.print("Digite o número de itens a serem criados:");
+
+            List<IItemBattle> itens = new ArrayList<IItemBattle>();
+            int n2 = s.nextInt();
+            Random r = new Random();
+            for(int j = 1; j <= n2; j++){
+                double findProb = r.nextDouble();
+                String name = String.format("Item "+ j);
+                String description = String.format("Item com findProb: %f",findProb);
+                IItemBattle itembattle = itemmanager.instantiateItemBattle(name, description,findProb, null);
+                System.out.println(itembattle);
+                itens.add(itembattle);
             }
+
             IItemBattle espada = itemmanager.instantiateItemBattle("Espada", "Fuderosa", 0.2, null);
 
             espada.addModAtt("Força", 10);
--- Awesome/core/src/com/umbra/mobModule/itemComponent/impl/ItemManager.java
+++ Awesome/core/src/com/umbra/mobModule/itemComponent/impl/ItemManager.java
@@ -7,6 +7,8 @@ import com.umbra.mapModule.IPosition;
 import com.umbra.mobModule.enums.Att;
 import com.umbra.mobModule.itemComponent.inter.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Component(
@@ -18,21 +20,22 @@ public class ItemManager extends ComponentBase implements IItemManager {
 
     private static void addModAtt(IItemBattle item, double rarity, Att att, Random r){
         double randRarity = r.nextDouble()/2 + 1/2;
-        item.addModAtt(att.getName(), rarity * randRarity * att.getIncrement());
+        double paramether = rarity * randRarity * att.getIncrement();
+        //paramether = Math.floor(paramether);
+        item.addModAtt(att.getName(), paramether);
     }
     public IItemBattle instantiateItemBattle(String name, String description, double findProb, IPosition pos){
         IItemBattle resp = new ItemBattle(name, description, findProb, pos);
         if(findProb != 0){
             double rarity = 1/findProb;
             Random r = new Random();
-            if(r.nextBoolean()) {
-                addModAtt(resp, rarity, Att.ATTACK, r);
-            }
-            if(r.nextBoolean()){
-                addModAtt(resp, rarity, Att.DEXTERITY, r);
-            }
-            if(r.nextBoolean()){
-                addModAtt(resp, rarity, Att.DEFENSE, r);
+            Att[] alowedToModify = {Att.ATTACK, Att.DEXTERITY, Att.DEFENSE};
+            boolean hasSome = false;
+            for(int j = 0; j < alowedToModify.length; j++){
+                if(r.nextBoolean()){
+                    addModAtt(resp, rarity, alowedToModify[j], r);
+                    hasSome = true;
+                }
             }
 
         }
