dc4c5c6620354ee4330f0312e2dc15bf0643e5de
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-06-09 21:41:34 -0300

    Adicao \n na introducao

---
+++ Awesome/core/src/com/umbra/dbModule/data/ObjectForTestsPurposeOnly.java
@@ -0,0 +1,13 @@
+package com.umbra.dbModule.data;
+
+public class ObjectForTestsPurposeOnly {
+	public int i;
+	
+	public ObjectForTestsPurposeOnly(int i){
+		this.i = i;
+	}
+	
+	public void get(){
+		System.out.println(this.i);
+	}
+}
