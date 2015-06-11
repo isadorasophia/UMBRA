16d58dfaaf15daa801f185ab8ff25ca40ae23474
Racoci <LRacoci@users.noreply.github.com>
2015-06-10 22:12:53 -0300

    Mais modificações de comentario

--- Awesome/core/src/com/umbra/mobModule/mobComponent/impl/PlayerInstantiator.java
+++ Awesome/core/src/com/umbra/mobModule/mobComponent/impl/PlayerInstantiator.java
@@ -16,20 +16,6 @@ import com.umbra.mobModule.mobComponent.inter.IPlayer;
  */
 
 public class PlayerInstantiator extends MobFactory {
-    
-    /*
-    atributos:
-        nivel = 1
-        currentHP
-        currentXP
-        defense
-        attack
-        dexterity
-        evasiveness
-        speed
-        luck
-        sanity = 0.0 a 1.0
-    */
 
     public static final Integer INITIAL_SIZE = 10;
 
